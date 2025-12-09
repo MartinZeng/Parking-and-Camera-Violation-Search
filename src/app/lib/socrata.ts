e62ea067c54c183440f37d62b2c45ae414e884ae
MartinZeng <martin.zeng621@gmail.com>
2025-12-09 15:26:55 -0500

    working on connecting frontend/backend

--- src/app/lib/socrata.ts
+++ src/app/lib/socrata.ts
@@ -1,10 +1,9 @@
 // this is where we will make queries and parse them
 import { Violation } from '../types/index';
 
-// const API_ENDPOINT = `https://data.cityofnewyork.us/api/v3/views/nc67-uf89/query.json?pageNumber=1&pageSize=10&app_token=$YUB3iukmGyYtZlDXruErL1D4m`;
 const API_ENDPOINT = `https://data.cityofnewyork.us/api/v3/views/nc67-uf89/query.json?pageNumber=1&pageSize=10&app_token=$YUB3iukmGyYtZlDXruErL1D4m&query=`;
-// const API_KEY = `ad8jckpni5pfuan1y66exlpy1`;
 const API_TOKEN = 'YUB3iukmGyYtZlDXruErL1D4m';
+// const API_KEY = `ad8jckpni5pfuan1y66exlpy1`;
 // const API_KEY_SECRET = '6bqdaavynupsxzpivz7s4ergdgos3xcg1yl9a1695vn6wop85v';
 
 // clean up license plate input submitted by user
@@ -12,7 +11,7 @@ function cleanPlate(plate: string): string {
   return plate.replace(/\s+/g, '').toUpperCase();
 }
 
-export async function searchByPlate(plate: string): Promise<any[]> {
+export async function searchByPlate(plate: string): Promise<Violation[]> {
   try {
     // Updating the license plate to the cleaned plate
     plate = cleanPlate(plate);
@@ -46,12 +45,12 @@ export async function searchByPlate(plate: string): Promise<any[]> {
   }
 }
 
-(async () => {
-  try {
-    const results = await searchByPlate('FFS4911');
-    console.log('Search Results:', results);
-  } catch (error) {
-    // Error already logged inside searchByPlate, but handle final exit here if needed
-    console.error('Script execution failed.');
-  }
-})();
+// (async () => {
+//   try {
+//     const results = await searchByPlate('t110874c');
+//     console.log('Search Results:', results);
+//   } catch (error) {
+//     // Error already logged inside searchByPlate, but handle final exit here if needed
+//     console.error('Script execution failed.');
+//   }
+// })();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,76 @@
-import Image from 'next/image';
+// import Image from 'next/image';
+'use client';
+import { useState } from 'react';
+import { searchByPlate } from './lib/socrata';
+import { Violation } from './types/index';
+import './globals.css';
 
 export default function Home() {
+  //defining state for input and fetched data
+  const [plate, setPlate] = useState('');
+  const [violations, setViolations] = useState<Violation[]>([]);
+  const [error, setError] = useState('');
+
+  // clears input when cancel button is clicked
+  function ClearInput() {
+    setPlate('');
+    setViolations([]);
+  }
+
+  // looks up info when search button is clicked
+  const LookUp = async () => {
+    if (!plate) return;
+    setError('');
+
+    try {
+      const data = await searchByPlate(plate);
+      setViolations(data);
+    } catch (e) {
+      setError('Search failed. Try again.');
+      setViolations([]);
+    }
+  };
+
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
-        <Image
-          className='dark:invert'
-          src='/next.svg'
-          alt='Next.js logo'
-          width={100}
-          height={20}
-          priority
-        />
-        <div className='flex flex-col items-center gap-6 text-center sm:items-start sm:text-left'>
-          <h1 className='max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50'>
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className='max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400'>
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              className='font-medium text-zinc-950 dark:text-zinc-50'
+    <main>
+      <div>
+        <header>NYC Parking and Camera Violations</header>
+        <form>
+          <label>Search Your License Plate:</label>
+          <label className='example-license'>
+            <small>Ex: EFG4321</small>
+          </label>
+          <div className='input'>
+            <input
+              className='border border-blue-300 '
+              type='text'
+              placeholder='License plate'
+              value={plate}
+              onChange={(event) => setPlate(event.target.value)}
+            ></input>
+          </div>
+          <div className='buttons'>
+            <button
+              className='bg-transparent hover:bg-teal-500 hover:text-white border border-teal-500 hover:border-transparent focus:ring-2 focus:outline-none focus:ring-teal-300 font-medium text-center py-2 px-4 rounded leading-5'
+              type='reset'
+              onClick={ClearInput}
             >
-              Templates
-            </a>{' '}
-            or the{' '}
-            <a
-              href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              className='font-medium text-zinc-950 dark:text-zinc-50'
+              Cancel
+            </button>
+            <button
+              className='bg-gradient-to-r from-teal-300 to-lime-300 hover:bg-gradient-to-l hover:from-teal-300 hover:to-lime-300 focus:ring-2 focus:outline-none focus:ring-lime-300 font-medium text-center py-2 px-4 rounded leading-5'
+              type='submit'
+              onClick={LookUp}
             >
-              Learning
-            </a>{' '}
-            center.
-          </p>
+              Search
+            </button>
+          </div>
+        </form>
+        <div className='violations-data'>
+          <p>Violations Data:</p>
+          <p>Insert Violations Data Here...</p>
         </div>
-        <div className='flex flex-col gap-4 text-base font-medium sm:flex-row'>
-          <a
-            className='flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]'
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image
-              className='dark:invert'
-              src='/vercel.svg'
-              alt='Vercel logomark'
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className='flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]'
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
